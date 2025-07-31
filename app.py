c24992da520a93dc76e1ba0e75fbccebaa5c15d3
Rakshak <rakshaksoni8@gmail.com>
2025-07-31 14:49:06 +0530

    date added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello I am rakshak soni!"
+    return "Hello I am rakshak soni date:31/07!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
